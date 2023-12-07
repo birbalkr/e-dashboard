c8f42634bd77bf93b66c597407900a02075ff56e
birbalkr <birbalkr1435@gmail.com>
2023-12-07 23:11:51 +0530

    create data file and import on index.js using api

---
+++ api/data.js
@@ -0,0 +1,7 @@
+const data = [
+    {nmae:'birbal kumar',email:'birbal@gmail.com'},
+    {nmae:'Rahul kumar',email:'rahul@gmail.com'},
+    {nmae:'sumit kumar',email:'sumit@gmail.com'}
+]
+
+module.exports=data;
--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 const http = require('http')
+const data =require('./data')
 
 http.createServer((req,res)=>{
 res.writeHead(200,{'Content-Type':'application\json'});
-res.write(JSON.stringify({nmae:'birbal kumar',email:'birbal@gmail.com'}));
+res.write(JSON.stringify(data));
 res.end();
 }).listen(8000);
