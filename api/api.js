eef2c0caf30ea80c588a25c4fb43643e8b299116
Birbal Kumar <birbalkr1435@gmail.com>
2024-01-03 01:26:41 +0530

    post get

--- api/api.js
+++ api/api.js
@@ -1,13 +1,20 @@
-const express = require('express')
-const dbConnect =require('./mongodb')
+const express = require('express');
+const dbConnect = require('./mongodb');
+const app = express();
 
-const app =express();
+app.use(express.json());
 
-app.get('/', async (req,res)=>{
-    let data=await dbConnect();
+app.get('/', async (req, res) => {
+    let data = await dbConnect();
     data = await data.find().toArray();
     console.log(data);
-    res.send({data})
+    res.send({ data })
 });
 
-app.listen(5000)
+app.post('/', async (req, rep) => {
+    let data = await dbConnect();
+    let result = await data.insertOne(req.body)
+    rep.send(result)
+}),
+
+    app.listen(5000)
